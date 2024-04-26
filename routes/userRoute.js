41024dd001f262754c22cc2e6684a5879bc8e6aa
affan34 <128572581+affan34@users.noreply.github.com>
2024-04-26 19:14:36 +0530

    bug fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { isAuthanticated } from "../middlewares/auth.js";
+import { isAuthanticated } from "../middlewares/Auth.js";
 import { newfile,getmyprofile,login,logout} from "../controllers/User.js";
 // for splitting routes
 const router = express.Router();
--- server.js
+++ server.js
@@ -5,6 +5,6 @@ import { connectdb } from "./data/database.js";
 connectdb();
 console.log(process.env.PORT);
 
-app.listen(POR,()=>{
+app.listen(PORT,()=>{
     console.log("server is working");
 })
