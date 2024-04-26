8911ef2926104c2f8d93378489711b424df6c7cf
affan34 <128572581+affan34@users.noreply.github.com>
2024-04-26 20:32:42 +0530

    bug fix

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ import { connectdb } from "./data/database.js";
 connectdb();
 console.log(process.env.PORT);
 
-app.listen(PORT,()=>{
+app.listen("4000",()=>{
     console.log("server is working");
 })
